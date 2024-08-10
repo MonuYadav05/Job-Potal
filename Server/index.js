6a2c33c93c6e4448ddbc97e838c004ddacb2905c
Monu Yadav <monuyadav60010@gmail.com>
2024-08-10 15:37:48 +0530

    mongo update

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 console.log(process.env.DB_USER, process.env.DB_PASS);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://Monuyadav60010:yplHM42Ga7opMkBS@job-portal.8grox.mongodb.net/?retryWrites=true&w=majority&appName=job-portal`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@job-portal.8grox.mongodb.net/?retryWrites=true&w=majority&appName=job-portal`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {

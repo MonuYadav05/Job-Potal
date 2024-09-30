a8f8c611dcbd8b4804a6c7974a1518a2434c4bb9
Monu Yadav <monuyadav60010@gmail.com>
2024-10-01 00:01:32 +0530

    w

--- Client/src/Components/ProfileDropdown.jsx
+++ Client/src/Components/ProfileDropdown.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { AiOutlineCaretDown } from "react-icons/ai";
@@ -10,6 +10,26 @@ const ProfileDropdown = () => {
   const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const ref = useRef();
+
+  // Close dropdown if clicked outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+
+    // Attach event listener to the document
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
+
+    return () => {
+      // Cleanup the event listener on component unmount
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, [ref]);
 
   const handleLogout = () => {
     dispatch(logout(navigate));
@@ -17,7 +37,7 @@ const ProfileDropdown = () => {
   };
 
   return (
-    <div className="mr-14 relative ">
+    <div className="mr-14 relative">
       <div
         className="flex flex-row justify-center items-center gap-1"
         onClick={() => {
@@ -26,30 +46,36 @@ const ProfileDropdown = () => {
       >
         <img
           src={user.image}
-          alt={`image of${user.firstName}`}
-          className="h-11 w-11 rounded-full object-cover  "
+          alt={`image of ${user.firstName}`}
+          className="h-11 w-11 rounded-full object-cover"
         />
         <AiOutlineCaretDown className="text-sm text-richblack-100" />
       </div>
 
-      {open && (
-        <div className="absolute flex flex-col top-[110%] right-[-10%] text-white font-semibold justify-center items-start  bg-blue rounded-lg p-3">
-          <Link
-            to={"/dashboard/my-profile"}
-            className="flex flex-row gap-x-1 py-[10px] px-[12px] text-lg justify-center hover:text-neutral-700 items-center"
-          >
-            <VscDashboard className="text-lg" />
-            Dashboard
-          </Link>
-          <div
-            onClick={handleLogout}
-            className="flex flex-row justify-center gap-x-1 py-[10px] px-[12px] text-lg items-center cursor-pointer hover:text-neutral-700"
-          >
-            <VscSignOut className="text-lg " />
-            Logout
-          </div>
+      <div
+        ref={ref}
+        className={`absolute flex flex-col top-[110%] right-[-10%] text-white font-semibold justify-center items-start bg-blue rounded-lg p-3 transform transition-all duration-500 ease-in-out ${
+          open
+            ? "opacity-100 scale-100"
+            : "opacity-0 scale-95 pointer-events-none"
+        }`}
+      >
+        <Link
+          onClick={() => setOpen(false)}
+          to="/dashboard/my-profile"
+          className="flex flex-row gap-x-1 py-[10px] px-[12px] text-lg justify-center hover:text-neutral-700 items-center"
+        >
+          <VscDashboard className="text-lg" />
+          Dashboard
+        </Link>
+        <div
+          onClick={handleLogout}
+          className="flex flex-row justify-center gap-x-1 py-[10px] px-[12px] text-lg items-center cursor-pointer hover:text-neutral-700"
+        >
+          <VscSignOut className="text-lg" />
+          Logout
         </div>
-      )}
+      </div>
     </div>
   );
 };
--- Client/src/Pages/MyJobs.jsx
+++ Client/src/Pages/MyJobs.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import { useSelector } from "react-redux";
 
 const MyJobs = () => {
